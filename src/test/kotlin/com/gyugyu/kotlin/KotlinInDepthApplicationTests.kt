630796211823a85cb3cacb8f2a4f3f3d146b9dbb
gyugyu90 <kh2000park@gmail.com>
2022-10-08 15:52:20 +0900

    initial commit

---
+++ src/test/kotlin/com/gyugyu/kotlin/KotlinInDepthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gyugyu.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinInDepthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
